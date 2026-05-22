d7e8c7ba59d59214b0298b961bf17e9c9f5f6bad
Igor Araújo do Nascimento <igor.araujo.nascimento@escola.pr.gov.br>
2026-05-22 11:13:24 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
+       String nome = "Igor";
+        int idade = 16;
+        double altura = 1.70;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
     }
 }
---
+++ programa.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Executando o programa java....
+java -cp "C:\Users\Aluno\Downloads\aulajava-main" Main
+pause
